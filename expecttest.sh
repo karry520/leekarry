de3dec9983a56fac0b06462f192b8a0972a125bb
karry520 <884816926@qq.com>
2019-06-24 09:33:20 +0800

    测试1

--- expecttest.sh
+++ expecttest.sh
@@ -2,14 +2,11 @@
 commit=$1
 git add *
 git commit -m "$commit"
-do
-{
-    expect << EOF
-    spawn git push
-    expect {
-        "Username*" {set timeout 2;send "karry520\r";exp_continue;}
-        "Password*" {set timeout 5;send "654123githubLKY\r";}
-    }
+expect << EOF
+spawn git push
+expect {
+    "Username*" {set timeout 2;send "karry520\r";exp_continue;}
+    "Password*" {set timeout 5;send "654123githubLKY\r";}
+}
 EOF
-}done
 
