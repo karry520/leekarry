4415a3486e280b3e2e2f86850cb16f3439393926
karry520 <884816926@qq.com>
2019-06-25 10:59:11 +0800

    EM算法

--- expecttest.sh
+++ expecttest.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 commit=$1
 git add *
+sleep 2
 git commit -m "$commit"
+sleep 2
 expect << EOF
-set timeout -1
+set timeout 60
 spawn git push
 expect "Username*"
 send "karry520\r"
 expect "Password*"
 send "654123githubLKY\r"
-sleep 25
+sleep 30
 interact
 EOF
 echo "完毕"
